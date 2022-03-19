52d24e8161888926186df9a9791a4dbc1cb3f83c
Lúcio Israel <97804377+LucioIsrael@users.noreply.github.com>
2022-03-19 18:29:33 -0300

    Criação da camada de layout

--- app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.example.marvelapp.framework.di
 
+import br.com.treino.core.data.network.interceptor.AutorizationInterceptor
 import com.example.marvelapp.BuildConfig
 import dagger.Module
 import dagger.Provides
@@ -9,11 +10,15 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private const val TIMEOUT_SECONDS = 15L
 
     //Evitar engenharia reversa
     @Provides
@@ -28,13 +33,24 @@ object NetworkModule {
     }
 
     @Provides
+    fun provideAuthorizationInterceptor(): AutorizationInterceptor {
+        return AutorizationInterceptor(
+            publicKey = "5737b77b5502c5c631b31460db25498d",
+            privateKey = "691a9084b270df42193b719376b00e61b4dc3bf6",
+            calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        )
+    }
+
+    @Provides
     fun provideOkHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor
+        loggingInterceptor: HttpLoggingInterceptor,
+        autorizationInterceptor: AutorizationInterceptor
     ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-            .readTimeout(15, TimeUnit.SECONDS)
-            .connectTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(autorizationInterceptor)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .build()
     }
 
@@ -54,5 +70,4 @@ object NetworkModule {
             .addConverterFactory(convertFactory)
             .build()
     }
-
 }
